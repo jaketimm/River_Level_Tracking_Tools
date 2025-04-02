6fe34a48dbed2d6a9e04055c224c4c59a607888f
jacob_timmerman <jaketimm.01@gmail.com>
2025-04-02 15:12:23 -0400

    add sampling rate option

--- River Level Tracker.py
+++ River Level Tracker.py
@@ -15,9 +15,10 @@ class MyApp(QWidget):
         self.initUI()
         self.time_period = 21  # default value of 21 days
         self.site_id = '04119070'  # default station choice
+        self.sample_interval = 6  # default sampling interval
 
     def initUI(self):
-        self.setGeometry(300, 300, 360, 200)
+        self.setGeometry(300, 300, 360, 250)  # Increased height to fit new dropdown
         self.setWindowTitle('River Data Downloader')
 
         self.site_id = ''
@@ -28,23 +29,30 @@ class MyApp(QWidget):
         self.combo_site.move(50, 50)
         self.combo_site.currentTextChanged.connect(self.updateSiteId)
 
+        # Add sampling interval dropdown
+        self.combo_sample = QComboBox(self)
+        self.combo_sample.addItems(['1 hour', '2 hours', '3 hours'])
+        self.combo_sample.move(130, 80)
+        self.combo_sample.setCurrentText('3 hours')  # Default value
+        self.combo_sample.currentTextChanged.connect(self.updateSampleInterval)
+
         # Add slider for time period
         self.slider = QSlider(Qt.Horizontal, self)
         self.slider.setMinimum(5)
         self.slider.setMaximum(21)
-        self.slider.setValue(21)  # default value
+        self.slider.setValue(21)
         self.slider.setTickPosition(QSlider.TicksBelow)
         self.slider.setTickInterval(1)
         self.slider.setFixedWidth(200)
-        self.slider.move(50, 100)
+        self.slider.move(50, 130)  # Moved down to accommodate sampling dropdown
         self.slider.valueChanged.connect(self.updateTimePeriod)
 
         # Add label to display selected days
         self.time_label = QLabel('21 days', self)
-        self.time_label.move(265, 100)
+        self.time_label.move(265, 130)  # Moved down
 
         self.button = QPushButton('Download and Display Data', self)
-        self.button.move(75, 150)
+        self.button.move(75, 180)  # Moved down
         self.button.clicked.connect(self.downloadAndDisplayData)
 
         self.show()
@@ -59,9 +67,13 @@ class MyApp(QWidget):
         self.time_period = value
         self.time_label.setText(f'{value} days')
 
+    def updateSampleInterval(self, text):
+        """Update the sampling interval based on dropdown selection."""
+        self.sample_interval = int(text.split()[0])  # Extract number from "X hours"
+
     def downloadAndDisplayData(self):
         download_river_data(self.site_id, self.time_period)
-        display_river_data(self.site_id)
+        display_river_data(self.site_id, self.sample_interval)
 
 '''
 Function: download_river_data
@@ -112,30 +124,30 @@ def download_river_data(station_id, num_days):
 
 '''
 Function: display_river_data
-Inputs: river station ID
+Inputs: river station ID, sampling interval
 Outputs: None
-Description: Loads, samples, and displays the river data for a selected river station
-Increments: 7 days - 2 hr sampling. 30 days - 6 hr sampling
+Description: Loads, samples data based on the sampling interval, 
+and displays the river data for a selected river station.
 '''
-def display_river_data(station_id):
-
+def display_river_data(station_id, sample_interval):
     # Read the RDB file into a DataFrame
     df = pd.read_csv('river_level_data.rdb', delimiter='\t', comment='#')
-    df = df.rename(columns={df.columns[4]: 'level'})  # rename column as it's a different number name for each station
+    df = df.rename(columns={df.columns[4]: 'level'})
 
     # Extract the time from the 'datetime' column as a string
     time = df['datetime'].str[11:]
 
-    # Select data for every 6 hrs
-    df_6h = df[time.isin(['00:00', '06:00', '12:00', '18:00'])]
+    # Generate sampling times based on selected interval
+    sample_hours = [f'{hour:02d}:00' for hour in range(0, 24, sample_interval)]
+    df_sampled = df[time.isin(sample_hours)]
+    
     # Convert level values from string to numeric
-    df_6h = df_6h.astype({'level': float}, errors='ignore')
-    df_6h.iloc[0, 4] = df_6h.iloc[1, 4]  # overwrite '14n' string at beginning of level data
+    df_sampled = df_sampled.astype({'level': float}, errors='ignore')
+    df_sampled.iloc[0, 4] = df_sampled.iloc[1, 4]  # overwrite '14n' string
 
-    fig, ax1 = plt.subplots(figsize=(12, 8))  # create a subplot and add labels
-    # Plot river level data
-    ax1.plot(df_6h['datetime'], df_6h['level'])
-    ax1.set_xlabel('Date (6 hr Increments)')
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+    ax1.plot(df_sampled['datetime'], df_sampled['level'])
+    ax1.set_xlabel(f'Date ({sample_interval} hr Increments)')
     ax1.set_ylabel('River Level (feet)')
 
     if station_id == '04119000':
@@ -144,7 +156,9 @@ def display_river_data(station_id):
         plt.title('Grand River at State Hwy M-11 at Grandville, MI - 04119070')
 
     ax1.tick_params(rotation=45)
-    ax1.set_xticks(ax1.get_xticks()[::4])  # only display one x label per day
+    # Adjust x-tick frequency based on sampling interval
+    tick_step = max(24 // sample_interval, 1)  # Show at least one label per day
+    ax1.set_xticks(ax1.get_xticks()[::tick_step])
     plt.tight_layout()
     plt.show()
 
